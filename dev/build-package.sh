1d0127f77f82ac220d5909da109a9d93e2d3c5bd
WilliamZhu <allwefantasy@gmail.com>
2020-07-01 10:17:19 +0800

    update

--- dev/build-package.sh
+++ dev/build-package.sh
@@ -5,3 +5,4 @@ cp  target/sql-code-intelligence-0.1.0.jar build/sql-code-intelligence-0.1.0
 cp -r config  build/sql-code-intelligence-0.1.0
 cd build
 tar cvf sql-code-intelligence-0.1.0.tar sql-code-intelligence-0.1.0
+scp build/sql-code-intelligence-0.1.0.tar mlsql2:/data/mlsql/releases/sql-code-intelligence/
